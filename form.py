dbebfdfee753632acafcb06b192b53d4a637c267
Yagi <yagi.t.ai@m.titech.ac.jp>
2023-07-24 01:24:15 +0900

    implemnt interactive UI

--- form.py
+++ form.py
@@ -1,49 +1,233 @@
+import pandas as pd
+
 #---戦績のフィルタを行うクラス
 class FormFilter:
 
-  def __init__(self, pre=None, post=None, smile=None, course=None, style=None, race=None):
-    self.pre = pre
-    self.post = post
-    self.smile = smile
-    self.course = course
-    self.style = style
-    self.race = race
+    def __init__(self, pre=None, post=None, smile=None, course=None, style=None, race=None):
+        self.pre = pre
+        self.post = post
+        self.smile = smile
+        self.course = course
+        self.style = style
+        self.race = race
   
-  def filter(self, form):
-    rslt = form
-    if self.pre is not None:
-      rslt = self.get_n_form(rslt, self.pre)
-    if self.smile is not None:
-      rslt = self.get_form_by_smile(rslt, self.smile)
-    if self.course is not None:
-      rslt = self.get_form_by_course(rslt, self.course)
-    if self.style is not None:
-      rslt = self.get_form_by_style(rslt, self.style)
-    if self.race is not None:
-      rslt = self.get_form_by_race(rslt, self.race)
-    if self.post is not None:
-      rslt = self.get_n_form(rslt, self.post)
-    return rslt
+    def filter(self, form):
+        rslt = form
+        if self.pre is not None:
+            rslt = self.get_n_form(rslt, self.pre)
+        if self.smile is not None:
+            rslt = self.get_form_by_smile(rslt, self.smile)
+        if self.course is not None:
+            rslt = self.get_form_by_course(rslt, self.course)
+        if self.style is not None:
+            rslt = self.get_form_by_style(rslt, self.style)
+        if self.race is not None:
+            rslt = self.get_form_by_race(rslt, self.race)
+        if self.post is not None:
+            rslt = self.get_n_form(rslt, self.post)
+        return rslt
+    
+    def __str__(self):
+        msg = 'FormFilter{'
+        if self.pre is None:
+            msg = msg + 'pre: - , '
+        else:
+            msg = msg + 'pre: '+str(self.pre)+' , '
+        if self.smile is None:
+            msg = msg + 'smile: - , '
+        else:
+            msg = msg + 'smile: '+str(self.smile)+' , '
+        if self.course is None:
+            msg = msg + 'course: - , '
+        else:
+            msg = msg + 'course: '+str(self.course)+' , '
+        if self.style is None:
+            msg = msg + 'style: - , '
+        else:
+            msg = msg + 'style: '+str(self.style)+' , '
+        if self.race is None:
+            msg = msg + 'race: - , '
+        else:
+            msg = msg + 'race: '+str(self.race)+' , '
+        if self.post is None:
+            msg = msg + 'post: -'
+        else:
+            msg = msg + 'post: '+str(self.post)
+        return msg + '}'
 
     #arr:SMILE区分、 subset of ['S','M','I','L','E']
     def get_form_by_smile(self, form, smile):
-    return form.query('SMILE in @smile')
+        return form.query('SMILE in @smile')
 
     #指定されたコースの戦績を抽出 course : string[]
     def get_form_by_course(self, form, course):
-    filter_list = [c[1:3] in course for c in form['開催']]
-    return form[filter_list]
+        filter_list = [c[1:3] in course for c in form['開催']]
+        return form[filter_list]
 
     #指定された脚質で走っていた戦績を抽出 style = subset of ['逃','先','差','追','マ']
     def get_form_by_style(self, form, style):
-    return form.query('style in @style')
+        return form.query('style in @style')
 
     #該当するレース名で抽出
     def get_form_by_race(self, form, race):
-    race = race.replace('(','\(').replace(')','\)')
-    return form.query('レース名.str.contains(@race)')
+        race = race.replace('(','\(').replace(')','\)')
+        return form.query('レース名.str.contains(@race)')
 
     #前n走
     def get_n_form(self, form, n):
-    n = max(len(form),n)
-    return form[0:n]
+        n = max(len(form),n)
+        return form[0:n]
+
+#レースの戦績関連の処理、馬Classのformを引数としてもらってなんやかんやしてbooleanを返す
+class FormEvaluator:
+
+    def __init__(self, win=None, show=None, last3f=None, margin=None, rank=None):
+        self.win    = win
+        self.show   = show
+        self.last3f = last3f
+        self.margin = margin
+        self.rank   = rank
+
+    def eval(self, form):
+        rslt = True
+        if len(form) == 0:
+            return False
+        if self.win is not None:
+            if type(self.win) == list:
+                rslt = rslt & is_win_rate_in(form, self.win[0],self.win[1])
+            else:
+                rslt = rslt & is_win_rate_in(form, self.win)
+        if self.show is not None:
+            if type(self.show) == list:
+                rslt = rslt & is_show_rate_in(form, self.show[0], self.show[1])
+            else:
+                rslt = rslt & is_show_rate_in(form, self.show)
+        if self.last3f is not None:
+            if type(self.last3f) == list:
+                rslt = rslt & is_3F_in(form, self.last3f[0], self.last3f[1])
+            else:
+                rslt = rslt & is_3F_in(form, th_max = self.last3f)
+        if self.margin is not None:
+            if type(self.margin) == list:
+                rslt = rslt & is_margin_in(form, self.margin[0], self.margin[1])
+            else:
+                rslt = rslt & is_margin_in(form, th_max = self.margin)
+        if self.rank is not None:
+            if type(self.rank) == list:
+                rslt = rslt & is_average_rank_in(form, self.rank[0], self.rank[1])
+            else:
+                rslt = rslt & is_average_rank_in(form, th_max = self.rank)
+        return rslt
+    
+    def __str__(self):
+        msg = 'FormEvaluator{'
+        if self.win is None:
+            msg = msg + 'win: - , '
+        elif type(self.win) == list:
+            msg = msg + 'win: {:.2f} ~ {:.2f} , '.format(self.win[0],self.win[1])
+        else:
+            msg = msg + 'win: {:.2f} , '.format(self.win)
+        if self.show is None:
+            msg = msg + 'show: - , '
+        elif type(self.show) == list:
+            msg = msg + 'show: {:.2f} ~ {:.2f} , '.format(self.show[0],self.show[1])
+        else:
+            msg = msg + 'show: {:.2f} , '.format(self.show)
+        if self.last3f is None:
+            msg = msg + 'last3f: - , '
+        elif type(self.last3f) == list:
+            msg = msg + 'last3f: {:.2f} ~ {:.2f} , '.format(self.last3f[0],self.last3f[1])
+        else:
+            msg = msg + 'last3f: {:.2f} , '.format(self.last3f)
+        if self.margin is None:
+            msg = msg + 'margin: - , '
+        elif type(self.margin) == list:
+            msg = msg + 'margin: {:.2f} ~ {:.2f} , '.format(self.margin[0],self.margin[1])
+        else:
+            msg = msg + 'margin: {:.2f} , '.format(self.margin)
+        if self.rank is None:
+            msg = msg + 'rank: -'
+        elif type(self.rank) == list:
+            msg = msg + 'rank: {:d} ~ {:d}'.format(self.rank[0],self.rank[1])
+        else:
+            msg = msg + 'rank: {:d}'.format(self.rank)
+        return msg + '}'
+
+
+#戦績から着順の回数だけを取り出す
+def make_rank_list(form):
+    ranks = [0]*19
+    for r in form['着 順']:
+        if str(r).isdigit():
+            i = int(r)
+            ranks[i-1] = ranks[i-1]+1
+        elif r == '中':
+            ranks[18] = ranks[18]+1
+    return ranks
+
+#着順の簡略表示を作る
+def make_simple_rank(form):
+    arr = make_rank_list(form)
+    rank = [0,0,0,0]
+    rank[0] = arr[0]
+    rank[1] = arr[1]
+    rank[2] = arr[2]
+    rank[3] = sum(arr[3:])
+    return str(rank[0])+'-'+str(rank[1])+'-'+str(rank[2])+'-'+str(rank[3])
+
+#勝率
+def is_win_rate_in(form, th_min = 0, th_max=1):
+    ranks = make_rank_list(form)
+    num = max(sum(ranks),1)
+    return th_min <= ranks[0] / num <= th_max
+
+#複勝率
+def is_show_rate_in(form, th_min=0, th_max=1):
+    ranks = make_rank_list(form)
+    num = max(sum(ranks),1)
+    return th_min <= sum(ranks[0:3]) / num <= th_max
+
+#上り3F
+def get_fastest_3F(form):
+    time_3f = min(form.dropna(subset = '上り')['上り'])
+    row_idx = form.dropna(subset = '上り')['上り'].idxth_min()
+    return row_idx, time_3f
+
+def is_3F_in(form, th_min=0, th_max=99):
+    form = form.dropna(subset = '上り')
+    if len(form) == 0:
+        return False
+    return th_min <= get_fastest_3F(form)[1] <= th_max
+
+#着差
+def get_average_margin(form):
+    margin = form.dropna(subset = '着差')['着差']
+    return sum(margin)/max(len(margin),1)
+
+def is_margin_in(form, th_min = -10.0 , th_max = 10.0):
+    form = form.dropna(subset = '着差')
+    if len(form) == 0:
+        return False
+    return th_min <= get_average_margin(form) <= th_max
+
+#順位の平均
+def get_average_rank(form):
+    len = 0
+    sum = 0
+    ranks = make_rank_list(form)
+    for i in range(18):
+        len = len + ranks[i]
+        sum = sum + ranks[i]*(i+1)
+    if len == 0:
+        return -1
+    return sum/len
+
+#平均順位の範囲
+def is_average_rank_in(form, th_min=0, th_max=18):
+    ave = get_average_rank(form)
+    return th_min <= ave <= th_max
+
+#レースの平均距離
+def get_average_distance(form):
+    dists = [int(d[1:]) for d in form['距離']]
+    return sum(dists)/len(dists)
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import raceList
 from race import Race
+from horse import Horse
+import form
+import matplotlib.pyplot as plt
 
 #回収率の計算
 def get_payout_ratio(horse_list):
@@ -7,16 +10,97 @@ def get_payout_ratio(horse_list):
     huku_ratio = sum([h.huku for h in horse_list])/max(len(horse_list),1)
     return {'単':tan_ratio, '複':huku_ratio}
 
+#レース結果の集計
+def make_result_list(hl):
+    results = [0]*19
+    for r in [h.get_chakujun() for h in hl]:
+        if str(r).isdigit():
+            i = int(r)
+            results[i-1] = results[i-1]+1
+        elif r == '中':
+            results[18] = results[18]+1
+    return results
+
+#レース結果の簡略表示
+def make_simple_result(hl):
+    arr = make_result_list(hl)
+    rank = [0,0,0,0]
+    rank[0] = arr[0]
+    rank[1] = arr[1]
+    rank[2] = arr[2]
+    rank[3] = sum(arr[3:])
+    return str(rank[0])+'-'+str(rank[1])+'-'+str(rank[2])+'-'+str(rank[3])
+
 #馬のリストを表示
 def dump_horses(hl):
     for h in hl:
         print(h)
     return
 
+# フィルター結果の表示
+def show_result(horse_list, filtered_list, label = True):
+    print_score(horse_list, filtered_list)
+
+    x =[]
+    y = []
+    l = []
+    for ho in filtered_list:
+        chakujun = str(ho.get_chakujun())
+        if chakujun == '除':
+            continue
+        else:
+            if not chakujun.isdigit():
+                chakujun = ho.result['頭 数'][0]
+            x.append(int(ho.get_ninki()))
+            y.append(int(chakujun))
+            l.append(ho.soup.find(class_ = "eng_name").get_text().replace('\n',''))
+
+    plt.scatter(x,y, alpha = .3)
+    plt.xlabel("favorite")
+    plt.ylabel("result")
+    plt.xticks(range(1,19))
+    plt.yticks(range(1,19))
+    for i, label in enumerate(l):
+        if label == True:
+            plt.text(x[i],y[i],label)
+    plt.show()
+    #plt.pause(2)
+
+def print_score(horse_list, filtered_list):
+    horses_win = [h for h in horse_list if str(h.get_chakujun()).isdigit() and int(h.get_chakujun()) == 1]
+    horses_show = [h for h in horse_list if str(h.get_chakujun()).isdigit() and int(h.get_chakujun()) <= 3]
+    num_filtered_win = len([h for h in filtered_list if str(h.get_chakujun()).isdigit() and int(h.get_chakujun()) == 1 ])
+    num_filtered_show = len([h for h in filtered_list if str(h.get_chakujun()).isdigit() and int(h.get_chakujun()) <= 3])
+    precise_win = num_filtered_win / max(len(filtered_list),1)
+    recall_win = num_filtered_win / max(len(horses_win),1)
+    precise_show = num_filtered_show / max(len(filtered_list),1)
+    recall_show = num_filtered_show / max(len(horses_show),1)
+    print(get_payout_ratio(filtered_list))
+    print('該当馬'+str(len(filtered_list))+'頭 ['+make_simple_result(filtered_list)+']')
+    print("1着: precise {:.2f}, recall {:.2f}".format(precise_win, recall_win))
+    print("複勝: precise {:.2f}, recall {:.2f}".format(precise_show, recall_show))
+
+
+def isfloat(s):  # 浮動小数点数値かどうかを判定する関数
+    try:
+        float(s)  # 試しにfloat関数で文字列を変換
+    except ValueError:
+        return False  # 失敗すれば False
+    else:
+        return True  # 上手くいけば True
+
+def input_error(stmt):
+    msg = '[main.py] input is not valid "'
+    for tk in stmt:
+        msg = msg +tk + ' '
+    msg = msg + '"'
+    print(msg)
+
+#---ここからメイン処理---
 race_name = "中京記念"
 race_list = raceList.get_race_list(race_name)
 
-YEARS_MAX = 5
+YEARS_MAX = 1
 races = []
 for years_ago in range(YEARS_MAX):
     race_id = race_list["ID"][years_ago]
@@ -25,7 +109,90 @@ for years_ago in range(YEARS_MAX):
 
 horse_list = []
 for r in races:
-    print('make Horse instance' + r.date[0:4])
+    print('make Horse instance... in ' + r.date[0:4])
     horse_list.extend(r.get_horses())
 
-dump_horses(horse_list)
+ff = form.FormFilter()
+fe = form.FormEvaluator()
+
+loop = True
+while(loop):
+    print(ff)
+    print(fe)
+
+    filtered_list = [h for h in horse_list if fe.eval(ff.filter(h.form))]
+    show_result(horse_list, filtered_list, False)
+
+    print('クエリは次の要素をスペース区切りで記入、&で複数可: [fe/ff] [変更したいフィールド] [変更後の閾値 1 or 2 個]')
+    #入力をもとにフィルタを変更する
+    query = input('>')
+    for stmt in query.split('&'):
+        stmt = [token for token in stmt.split(' ') if token != '']
+        if len(stmt) >= 1 and stmt[0] == 'q':
+            loop = False
+            continue
+
+        if len(stmt) < 2:
+            input_error(stmt)
+            continue
+        
+        if stmt[0] == 'ff':
+            if stmt[1] == 'pre':
+                if len(stmt)== 2:
+                    ff.pre = None
+                else:
+                    ff.pre = float(stmt[2])
+            elif stmt[1] == 'smile':
+                if len(stmt)== 2:
+                    ff.smile = None
+                else:
+                    ff.smile = stmt[2]
+            elif stmt[1] == 'course':
+                if len(stmt)== 2:
+                    ff.course = None
+                else:
+                    ff.course = stmt[2]
+            elif stmt[1] == 'style':
+                if len(stmt)== 2:
+                    ff.style = None
+                else:
+                    ff.style = stmt[2]
+            elif stmt[1] == 'race':
+                if len(stmt)== 2:
+                    ff.race = None
+                else:
+                    ff.race = stmt[2]
+            elif stmt[1] == 'post':
+                if len(stmt)== 2:
+                    ff.post = None
+                else:
+                    ff.post = float(stmt[2])
+            elif stmt[1] == 'crear':
+                ff.post = None
+                ff.pre = None
+                ff.course = None
+                ff.race = None
+                ff.smile= None
+                ff.course = None
+            else:
+                input_error(stmt)
+        elif stmt[0] == 'fe':
+            if stmt[1] == 'clear':
+                fe.win = None
+                fe.show = None
+                fe.rank = None
+                fe.last3f = None
+                fe.margin = None
+            else:
+                try:
+                    getattr(fe, stmt[1])
+                    if len(stmt) == 3:
+                        setattr(fe, stmt[1], float(stmt[2]))
+                    elif len(stmt) == 4:
+                        setattr(fe, stmt[1],[float(stmt[2]),float(stmt[3])])
+                    else:
+                        input_error(stmt)
+                except AttributeError:
+                    input_error(stmt)
+        else:
+            input_error(stmt)
